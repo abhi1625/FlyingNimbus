f98accb3a1d956b4877522e40a8f53427c72bd5a
abhi1625 <abhi1625@terpmail.umd.edu>
2019-12-05 08:59:49 -0500

    created basic skeleton for the package

---
+++ src/StateEstimate.py
@@ -0,0 +1,31 @@
+import time
+import rospy
+import numpy as np 
+from geometry_msgs.msgs import Twist, Pose
+from nav_msgs.msg import Odometry
+from bebop_msgs.msg import Ardrone3PilotingStateAttitudeChanged
+
+class StateEstimate:
+    def __init__(self):
+        self.current_pos = np.array((3,))
+        self.current_yaw = 0.0
+        self.current_state = Pose()
+
+        # Subscribers for bebop odom estimates
+        self.bebop_odomSub = rospy.Subscriber('/bebop/odom', Odometry, self.odom_callback)
+        # Subscriber for heading
+        self.bebop_yawSub = rospy.Subscriber('/bebop/states/ardrone3/PilotingState/AttitudeChanged',Ardrone3PilotingStateAttitudeChanged, self.yaw_callback)
+        # Current state publisher 
+        self.state_pub = rospy.Publisher('/current_state', Pose, queue_size=1)
+
+    def odom_callback(self, data):
+        self.current_pos[0] = data.twist.twist.linear.x*0.2 + self.current_pos[0]
+        self.current_pos[1] = data.twist.twist.linear.y*0.2 + self.current_pos[1]
+        self.current_pos[2] = data.twist.twist.linear.z*0.2 + self.current_pos[2]
+    
+    def yaw_callback(self, data):
+        self.current_yaw = data.yaw
+
+def main():
+    rospy.init_node('estimateion_node', anonymous=True)
+    
